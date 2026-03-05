3d62329ef1a53df209d7636c8ade30548df162cb
Jack Thornburg <jackrt@unc.edu>
2026-03-05 20:51:02 +0000

    created struct to store regional symbol models

--- assgn1/src/challenge.rs
+++ assgn1/src/challenge.rs
@@ -1,15 +1,32 @@
 use toy_ac::symbol_model::VectorCountSymbolModel;
-use rangemap::RangeMap;
 
-pub struct RegionEncodingMap<T: std::cmp::Eq>{
-    map: RangeMap<u32, VectorCountSymbolModel<T>>,
+pub struct RegionEncodingMap {
+    symbol_models: Vec<VectorCountSymbolModel<u32>>,
     height: u32,
-    width:u32,
-    subdivisions: u32
+    width: u32,
+    resolution: u32,
 }
 
-impl<T: std::cmp::Eq> RegionEncodingMap<T> {
-    pub fn get_symbol_model(&self, key: u32) -> &VectorCountSymbolModel<T> {
-        return self.map.get(&key).unwrap();
+impl RegionEncodingMap {
+    pub fn new(height: u32, width: u32, resolution: u32) -> Self {
+        let symbol_models = (0..resolution.pow(2))
+            .map(|_| VectorCountSymbolModel::new((0..=255).collect()))
+            .collect::<Vec<_>>();
+
+        Self {
+            symbol_models,
+            height,
+            width,
+            resolution
+        }
+    }
+    pub fn get_symbol_model(&self, pixel: (u32, u32)) -> &VectorCountSymbolModel<u32> {
+        let (r, c) = pixel;
+
+        let r_offset = (self.resolution * r) / (self.height + 1);
+        let c_offset = (self.resolution * c) / (self.width + 1);
+        let i = (self.resolution * r_offset + c_offset) as usize;
+
+        return &self.symbol_models[i];
     }
 }
